fefa6cc2b233fb93b0e2caa0d8ad9a793218beaf
CsanmartinFCL <122047434+CsanmartinFCL@users.noreply.github.com>
2023-02-03 15:19:59 -0300

    Delete webxpcPruebaBib directory

--- webxpcPruebaBib/src/main/java/com/facele/docele/webxpc/Constantes.java
+++
@@ -1,6 +0,0 @@
-package com.facele.docele.webxpc;
-
-public class Constantes {
-
-	public static enum EstadoArchivo {LOCAL,REMOTO};
-}
--- webxpcPruebaBib/src/main/java/com/facele/docele/webxpc/service/LoadService.java
+++
@@ -1,8 +0,0 @@
-package com.facele.docele.webxpc.service;
-
-import io.facele.facilito.dto.v10.xpc.InputUploadCrear;
-
-public interface LoadService {
-	String crear(final InputUploadCrear inputDTO);
-
-}
